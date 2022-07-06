1e185f3949b2d2ffa84edced481c908feecb1898
MyungHwanKim <kmh9250@naver.com>
2022-07-06 23:48:05 +0900

    main page 설정

---
+++ src/main/java/com/example/lms/MainController.java
@@ -0,0 +1,16 @@
+package com.example.lms;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Controller
+public class MainController {
+	
+	@RequestMapping("/")
+	public String index() {		
+		return "index";
+	}
+}

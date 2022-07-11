aafb3bb8541ec6a469282b82838e0f68215eb6b9
MyungHwanKim <kmh9250@naver.com>
2022-07-11 18:50:56 +0900

    로그인할 때 실패하는 예외 구현

---
+++ src/main/java/com/example/lms/member/exception/MemberNotEmailAuthException.java
@@ -0,0 +1,8 @@
+package com.example.lms.member.exception;
+
+public class MemberNotEmailAuthException extends RuntimeException {
+	public MemberNotEmailAuthException(String error) {
+		super(error);
+	}
+
+}

a32f3aaa7f483ef937be0b9224ec8857a4f7fdc0
MyungHwanKim <kmh9250@naver.com>
2022-07-13 18:07:34 +0900

    code 정리(CommonParam으로 묶기, BaseController로 묶기)

--- src/main/java/com/example/lms/admin/controller/AdminMemberController.java
+++ src/main/java/com/example/lms/admin/controller/AdminMemberController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import com.example.lms.admin.dto.MemberDto;
 import com.example.lms.admin.model.MemberInput;
 import com.example.lms.admin.model.MemberParam;
+import com.example.lms.course.controller.BaseController;
 import com.example.lms.member.service.MemberService;
 import com.example.lms.util.PageUtil;
 
@@ -17,7 +18,7 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Controller
-public class AdminMemberController {
+public class AdminMemberController extends BaseController {
 	
 	private final MemberService memberService;
 
@@ -25,19 +26,21 @@ public class AdminMemberController {
 	public String list(Model model, MemberParam memberParam) {
 		
 		memberParam.init();
-		
 		List<MemberDto> members = memberService.list(memberParam);
 		
 		long totalCount = 0;
 		if (members != null && members.size() > 0) {
 			totalCount = members.get(0).getTotalCount();
 		}
-		String queryString = memberParam.getQueryString();
 		
-		PageUtil pageUtil  = new PageUtil(totalCount, memberParam.getPageSize(), memberParam.getPageIndex(), queryString);
+		String queryString = memberParam.getQueryString();
+		String pageHtml  = getPaperHtml(totalCount, 
+										memberParam.getPageSize(), 
+										memberParam.getPageIndex(), 
+										queryString);
 		model.addAttribute("list", members);
 		model.addAttribute("totalCount", totalCount);
-		model.addAttribute("pager", pageUtil.pager());
+		model.addAttribute("pager", pageHtml);
 		
 		return "admin/member/list";
 	}

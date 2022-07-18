2f14a9980811e6e7c522860de7c7028116bfcfb9
MyungHwanKim <kmh9250@naver.com>
2022-07-18 14:11:24 +0900

    Main page 배너 노출 구현

--- src/main/java/com/example/lms/main/controller/MainController.java
+++ src/main/java/com/example/lms/main/controller/MainController.java
@@ -1,9 +1,10 @@
 package com.example.lms.main.controller;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.lms.banner.service.BannerService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -11,8 +12,11 @@ import lombok.RequiredArgsConstructor;
 @Controller
 public class MainController {
 	
+	private final BannerService bannerService;
+	
 	@RequestMapping("/")
-	public String index(HttpServletRequest request) {
+	public String index(Model model) {
+		model.addAttribute("list", bannerService.listAll());
 		return "index"; 
 	}
 	
